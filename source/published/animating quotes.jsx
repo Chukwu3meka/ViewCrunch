b0f1133d37ee8ef869286c9a55670d8786aa4d58
MaduekwePedro <maduekwepedro@gmail.com>
2021-05-17 22:20:26 +0100

    added blog post to firestore

---
+++ source/published/animating quotes.jsx
@@ -0,0 +1,80 @@
+const title = "Animating Quotes for the day",
+  description =
+    "1. Seal team six: You think you're so important; put your finger in a bucket of water, you pull it out and see how fast that hole fills up. Everyone is replaceable including me. 2. Brad Traversy: Wanting others to",
+  keywords = "quotes, inspiration, career",
+  content = (
+    <>
+      <ol>
+        <li>
+          Denzel Washington :::
+          <br />I don't scratch my head unless it itches and i don't dance unless i hear some music.
+        </li>
+        <li>
+          Seal team six :::
+          <br />
+          You think you're so important; put your finger in a bucket of water, you pull it out and see how fast that hole fills up.
+          Everyone is replaceable including me.
+        </li>
+        <li>
+          Pedro :::
+          <br />
+          The number of dead people you know will grow overtime except you die now. We'll all die one day.
+        </li>
+        <li>
+          Brad Traversy :::
+          <br />
+          Wanting others to fail won't bring you success, it will bring you more failure. I believe what you give out you get back.
+        </li>
+        <li>
+          Pedro :::
+          <br />
+          You will treat those you love the same way you treat those you hate but in a different way, So pay more attention to how you
+          treat your haters.
+        </li>
+        <li>
+          Napoleon Hill :::
+          <br />
+          Whatever the mind of man can conceive, it can achieve.
+        </li>
+        <li>
+          Walt Disney :::
+          <br />
+          The way to get started is to quit talking and start doing.
+        </li>
+        <li>
+          Pedro :::
+          <br />
+          It's only in darkness, we need light. There will always be something to think of at any moment.
+        </li>
+        <li>
+          Templar :::
+          <br />
+          You have to learn to switch off, relax, not take it so seriously, enjoy it more, Put things into perspective.
+        </li>
+        <li>
+          Gandhi :::
+          <br />
+          You must be the change you wish to see in the world.
+        </li>
+        <li>
+          Confucius :::
+          <br />
+          It does not matter how slowly you go, so long as you do not stop.
+        </li>
+        <li>
+          Albert Einstein :::
+          <br />A ship is always safe at the shore, but that's not what its built for.
+        </li>
+        <li>
+          Abraham Lincoln :::
+          <br />
+          If i had eight hours to chop down a tree, i'll spend six hours sharpening my axe.
+        </li>
+        <li>
+          Unknown :::
+          <br />
+          If the only tool you have is hammer, you tend to see every problem as a nail.
+        </li>
+      </ol>
+    </>
+  );
