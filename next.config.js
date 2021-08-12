25216d1599dc2370de84c5b576ccc2833253ddb8
MaduekwePedro <maduekwepedro@gmail.com>
2021-08-12 19:12:41 +0100

    webpack 5 enabled by default

--- next.config.js
+++ next.config.js
@@ -12,21 +12,10 @@ module.exports = {
     FIREBASE_PRIVATE_KEY:
       "-----BEGIN PRIVATE KEY-----\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQDHAy475Cg1pozE\nUYETWZUZUzLlFeL9rCPle689rc+6c+k2hdIkX4JGuNapMQf7BLgtUZ8o7fY29He1\nP9HPazVpjZVnVoOpzAjSkv3HZ6mlkEvN4yu6F/MRe7glIUD4VdiaFs7fTu7o9E0p\nGF/yp49lvZYfcXmJZDmFttbdUb8xeTt/UdzDhtdpmAb+ASgGZ4OABOmum79wWqs+\ngGUtJt4RdPvCEbt6PKFxcdXCexd/8sWB+pV7ytrFc5aqTMWGdxRcqIlO89Jsvt5H\n+gWSpmplkEuXU5hQInMZK5ax+BpzqzPQYNhBpBOCYq12SQZIVv2fSUC4Ym3YcdSi\n6ixJsvrLAgMBAAECggEADM9hGfzRM8bhN7/RUagKB2PKGmHixxZeBycDfDJkdSmB\n4P8TLEEzlw7Nzn03+MnwewZOeul6WEY7MGEHxdSuTvmMnOa+Od9G0BsjF8NN8bjM\nkFenmGZzQHRYwZcW4gVQmHaQYWJo0XjsQJFvl6R+f/uInsSEoLnhVACVaxCYgHwi\nddZ4foBflRLwgEOn6f4cHhFbLVVAv390yU7J/JttPUQk9Tf1X6kunzQUV4w6C626\nHfNJSC+RdFB1vYxPV4Wdu8Kr3+XOLPaQlqmpGRE5f53z3Ytua+3qn8H6uVVfHCDK\nwgEUQkPqzYv9lUudAXF/jnnBsHvllDC22e16T+8SLQKBgQD/u8op3G99aPTHfwIW\nsjXOJij0Be3qJbp8LkRsFPOHsn+NHqJoweGzDhDmLPmt2QmyUjQgZMxIjlp2UT5B\nen2YpfLHZWuiQMioiMDIq54mBud7hpEQcIm4gePN8iOiZiyte2QCbNKTQZ4KO/EJ\n9KNkhn+KfdqzSHXe2kXsMb8b7wKBgQDHOEMS/8xPNChyfVFoEbZUNrlidK0Zm5E1\nLTTyLsfX1LHgR5AOn84A1+mBQDVeXszO3/MrDkPN5/hSn/6XuAy0djUgk6bUZku+\nD+tsfv+djlxhhxut3uiWUi8lq2dUjU2PDXrsQ47VlBInp95LNtCyUCThktJeGnrp\nJxP1AEri5QKBgQCUkcq/SxtSZnletQDHorzziOPd8NvpXgXbQx3pFLruqNrFZ3i/\nF3RCC/fxYUukniSt/mfeMfEkbd9MQaLDc6An4LEM+nTvjGeG0fv+GQYF3Zj6AJGi\nVV8NXBi2eugM3wMxvi+54PLtLf/cvjooqIGMUz8hfEBkS8J+t5xLvhLi7QKBgQDD\nQYB0GaQ8qMlP1UW5c0q55Vzzg5m49IFwlhtDltgl0QB56onl23jD2Nqy5KSqFs4z\noMmrlHKeWFJJYSjduPUy1nyhrjWgDZcE9INwtlSALuyaZhqZ4lUiJ/Cvwfwnocq3\nf6XP9TMEX/kfRueX3mTTr7tGH2nx85At/jAQPDYphQKBgAlhc6YWF/kXa1IN/4Zr\n1Y8Cr4Mz5KIjKvJ/pxlNOPilO7cnaD1J1K+fJjexew302+gWFvLEoDkcFm5yUqSx\nyANMYWPKlHqw1u/McnFyl/9Fc5VyAKXgkxbCtbrdtMddompHgriEDWOQBKhKqdaD\nJ0E+aCCWGVx3QOA+gUB943cV\n-----END PRIVATE KEY-----\n",
     GA_TRACKING_ID: "G-0BJL8GBSKY",
-    // GA_TRACKING_ID: "UA-168019441-1",
   },
   target: "serverless",
   images: {
-    // domains: ["firebase.com"],
     domains: ["platform-lookaside.fbsbx.com", "firebasestorage.googleapis.com", "scontent-atl3-2.xx.fbcdn.net"],
-
-    // firebasestorage.googleapis.com/v0/b/viewcrunch-2018.appspot.com/o/images%2F%40%40maduekwepedro%2Ffirst%20post%20in%20catholic%20church%400.png?alt=media&token=bd060443-355c-4ddd-bc82-d0572f4a199a) on `next/image`, hostname "firebasestorage.googleapis.com" is not configured under images in your `next.config.js`
-
-    // or
-    // Error: Invalid src prop (https://scontent-atl3-2.xx.fbcdn.net/v/t1.30497-1/c29.0.100.100a/p100x100/84628273_176159830277856_972693363922829312_n.jpg?_nc_cat=1&ccb=1-4&_nc_sid=12b3be&_nc_ohc=GsA3y-ZkuUoAX_VFkof&_nc_ht=scontent-atl3-2.xx&edm=AP4hL3IEAAAA&oh=19bbbd2923855115071affa90dc53b68&oe=6138E675) on `next/image`, hostname "scontent-atl3-2.xx.fbcdn.net" is not configured under images in your `next.config.js`
-    // See more info: https://nextjs.org/docs/messages/next-image-unconfigured-host
-    // Source
   },
-  // future: {
-  //   // webpack5: true,
-  // },
+  // webpack5: true,
 };
