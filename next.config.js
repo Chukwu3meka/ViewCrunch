dfee207befbedeae13765ef65c887afbc9b8acbc
Chukwuemeka Maduekwe <chukwuemeka@viewcrunch.com>
2022-01-25 22:23:50 +0100

    reenable nextconfig for prod

---
+++ next.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  env: {},
+  target: "serverless",
+  images: {
+    domains: ["platform-lookaside.fbsbx.com", "firebasestorage.googleapis.com", "scontent-atl3-2.xx.fbcdn.net"],
+  },
+  // webpack5: true,
+};
