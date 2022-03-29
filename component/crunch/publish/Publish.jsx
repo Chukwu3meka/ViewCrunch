35550cf962a3985cce92e31afa494521195db370
Chukwuemeka Maduekwe <chukwuemeka@viewcrunch.com>
2022-03-29 20:59:42 +0100

    created upload component

--- component/crunch/publish/Publish.jsx
+++ component/crunch/publish/Publish.jsx
@@ -1,8 +1,22 @@
 import { styles } from ".";
-import { Footer, NavContainer } from "@component/layout";
-import { Grid, Typography } from "@mui/material";
+import { NavContainer } from "@component/layout";
+import { Box, Grid, Input, TextField, Tooltip, Typography } from "@mui/material";
+import AddPhotoIcon from "@mui/icons-material/AddAPhoto";
+import Fab from "@mui/material/Fab";
+import DeleteForeverIcon from "@mui/icons-material/DeleteForever";
 
-const Publish = ({ crunches }) => (
+const Publish = ({
+  //
+  titleChangeHandler,
+  title,
+  formatContentArray,
+  setContentArray,
+  contentArray,
+  contentText,
+  setContentText,
+  content,
+  imageHandler,
+}) => (
   <Grid container>
     {/* style={{ maxWidth: "1200px", margin: "auto" }} */}
     <NavContainer>
@@ -13,17 +27,91 @@ const Publish = ({ crunches }) => (
       </div>
     </NavContainer>
     <Grid item xs={12} sm={12} md={8}>
-      <div className={styles.publish}>
-        <TextField
-          fullWidth
+      <Box p={1} className={styles.publish}>
+        <Input
           autoFocus
-          // error={title.length && !!titleHandler(title) ? true : false}
-          label="Title"
-          placeholder="Title of view"
-          // value={title}
-          // onChange={(e) => titleHandler(e.target.value.trimStart().replace(/\s+/g, " "))}
+          fullWidth
+          value={title}
+          disableUnderline={true}
+          placeholder="View Title"
+          onChange={titleChangeHandler}
+          sx={{ fontSize: 35, caretColor: "#1197c0" }}
         />
-      </div>
+
+        {/* content */}
+
+        <div id={styles.content}>
+          {formatContentArray()?.map((x, index, loopArray) =>
+            typeof x === "object" ? (
+              <span key={index}>
+                <Tooltip
+                  title="Delete this photo"
+                  onClick={() => {
+                    setContentArray(loopArray.filter((selected, selectedIndex) => selectedIndex !== index));
+                  }}>
+                  <Fab color="secondary" size="small" aria-label="add-image">
+                    <DeleteForeverIcon />
+                  </Fab>
+                </Tooltip>
+                <img src={x?.image} />
+              </span>
+            ) : (
+              <Input
+                fullWidth
+                multiline
+                disableUnderline={true}
+                key={index}
+                value={x}
+                fullWidth
+                // InputProps={{ disableUnderline: true }}
+                multiline
+                onChange={(e) => {
+                  const tempContent = [...formatContentArray()];
+                  tempContent[index] = e.target.value;
+                  setContentArray([...tempContent]);
+                }}
+              />
+            )
+          )}
+
+          {typeof contentArray[contentArray?.length - 1] !== "String" && (
+            <Input
+              fullWidth
+              multiline
+              onChange={(e) => setContentText(e.target.value)}
+              value={contentText}
+              disableUnderline={true}
+              sx={{
+                fontSize: "1.1em",
+                caretColor: "#1197c0",
+              }}
+              placeholder="Tell the world your view..."
+            />
+          )}
+        </div>
+      </Box>
+
+      {/* <FixedIcon icon="publish" clickHandler={{ image: imageHandler, down: () => scroll2Ref("end"), up: () => scroll2Ref("start") }} /> */}
+
+      <Fab color="primary" aria-label="add-image" sx={{ position: "fixed", bottom: 16, right: 16, overflow: "hidden" }}>
+        <input
+          type="file"
+          onChange={imageHandler}
+          accept=".jpg, .jpeg, .png"
+          accept="image/*"
+          multiple
+          style={{
+            position: "absolute",
+            top: "-35px",
+            left: 0,
+            height: "calc(100% + 36px)",
+            width: "calc(100% + 5px)",
+            outline: "none",
+          }}
+        />
+
+        <AddPhotoIcon />
+      </Fab>
     </Grid>
   </Grid>
 );
--- component/crunch/publish/PublishContainer.jsx
+++ component/crunch/publish/PublishContainer.jsx
@@ -1,7 +1,104 @@
+import { imageObject } from "@utils/clientFunctions";
+import Joi from "joi";
+import { useSnackbar } from "notistack";
+import { useState } from "react";
+
 import { Publish } from ".";
 
-const PublishContainer = ({ crunches }) => {
-  return <Publish />;
+const PublishContainer = (props) => {
+  const [title, setTitle] = useState("");
+  const [content, setcontent] = useState("second");
+  // const [titleErr, setTitleErr] = useState(false);
+
+  const titleChangeHandler = (e) => setTitle(e.target.value);
+
+  const { viewToBeModified = {}, crunch, published, moderator } = props,
+    { enqueueSnackbar } = useSnackbar(),
+    [loading, setLoading] = useState(false),
+    [preview, setPreview] = useState(false),
+    [contentText, setContentText] = useState(""),
+    // [title, setTitle] = useState(viewToBeModified.title || ""),
+    [keywords, setKeywords] = useState(viewToBeModified.keywords || ""),
+    [description, setDescription] = useState(viewToBeModified.description || ""),
+    [retouchWarning, setRetouchWarning] = useState(viewToBeModified.title ? true : false),
+    [contentArray, setContentArray] = useState(viewToBeModified.content ? [viewToBeModified.content] : []);
+
+  const formatContentArray = () => {
+    const newContentArray = [];
+
+    [...contentArray]?.forEach((value, index, loopArr) => {
+      if (typeof value === "object") {
+        newContentArray.push(value);
+      } else {
+        if (typeof newContentArray[newContentArray.length - 1] === "object" || index === 0) {
+          newContentArray.push(value);
+        } else {
+          newContentArray[newContentArray.length - 1] = `${newContentArray[newContentArray.length - 1]}\n${value}`;
+        }
+      }
+    });
+    () => setContentArray(newContentArray);
+    return newContentArray;
+  };
+
+  const imageHandler = async (e) => {
+    const contentImagesArray = e.target.files;
+    const contentImages = [];
+    if (contentImagesArray?.length) {
+      for (const image of contentImagesArray) {
+        const imageSplit = image?.name?.split(".");
+        if (!["png", "jpg", "jpeg"].includes(imageSplit[imageSplit.length - 1])) return;
+        contentImages.push({ image: await imageObject(image) });
+      }
+      if (contentText) {
+        contentArray?.length
+          ? setContentArray([...contentArray, contentText, ...contentImages])
+          : setContentArray([contentText, ...contentImages]);
+        setContentText("");
+      } else {
+        contentArray?.length ? setContentArray([...contentArray, ...contentImages]) : setContentArray([...contentImages]);
+      }
+    }
+  };
+
+  return (
+    <Publish
+      //
+
+      {...{
+        // title,
+        // crunch,
+        // classes,
+        // loading,
+        // preview,
+
+        // contentArray,contentText,setContentText
+
+        // keywords,
+        // scrollRef,
+        // moderator,
+        // setPreview,
+        // scroll2Ref,
+        contentText,
+        // description,
+        imageHandler,
+        // titleHandler,
+        contentArray,
+        // retouchWarning,
+        setContentText,
+        // previewHandler,
+        setContentArray,
+        // keywordsHandler,
+        // setRetouchWarning,
+        // descriptionHandler,
+        formatContentArray,
+        // oldContent: viewToBeModified.content,
+      }}
+      titleChangeHandler={titleChangeHandler}
+      title={title}
+      // content={content}
+    />
+  );
 };
 
 export default PublishContainer;
